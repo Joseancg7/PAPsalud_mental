6fc1d1dde91d6240b5c6bfbfed2d2fc6bdaa605c
renattaGS <renattags20@gmail.com>
2021-11-07 10:34:26 -0600

    Modelo clustering

--- Main.py
+++ Main.py
@@ -1,5 +1,9 @@
+from numpy import mod
 import pandas as pd
 import tslearn
+from tslearn.preprocessing import TimeSeriesScalerMeanVariance
+from tslearn.clustering import TimeSeriesKMeans, KShape, silhouette_score
+
 
 # Import data
 data_o = pd.read_excel('Data\\Archivo_1_Datos_TS.xlsx')
@@ -9,3 +13,12 @@ data_o = pd.read_excel('Data\\Archivo_1_Datos_TS.xlsx')
 data_c = data_o.dropna(subset=['Fecha'])
 
 data_c = data_c.fillna(method='ffill')
+
+X_train = TimeSeriesScalerMeanVariance().fit_transform(data_c.drop('Fecha', axis=1))
+
+silhouette = []
+
+for i in range(1,10):
+    model = TimeSeriesKMeans(n_clusters=i, metric="dtw", max_iter=10).fit(X_train)
+    labels = model.labels_
+    silhouette.append(silhouette_score(X_train, labels, metric="dtw") )
