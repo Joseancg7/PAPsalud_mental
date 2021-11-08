ddfc692184a5fa8d8793212273f65d2957300575
renattaGS <renattags20@gmail.com>
2021-11-08 13:48:23 -0600

    analisis por cluster draft

--- Main.py
+++ Main.py
@@ -1,8 +1,7 @@
-from numpy import mod
 import pandas as pd
-import tslearn
+import numpy as np
 from tslearn.preprocessing import TimeSeriesScalerMeanVariance
-from tslearn.clustering import TimeSeriesKMeans, KShape, silhouette_score
+from tslearn.clustering import TimeSeriesKMeans, silhouette_score
 
 
 # Import data
@@ -11,14 +10,106 @@ data_o = pd.read_excel('Data\\Archivo_1_Datos_TS.xlsx')
 # Drop rows with missing dates
 
 data_c = data_o.dropna(subset=['Fecha'])
-
+data_c.set_index('Fecha', inplace=True)
 data_c = data_c.fillna(method='ffill')
 
-X_train = TimeSeriesScalerMeanVariance().fit_transform(data_c.drop('Fecha', axis=1))
+# Estandarize variables
+X_train = TimeSeriesScalerMeanVariance().fit_transform(data_c)
 
+# Initialize silhouette resultas
 silhouette = []
 
-for i in range(1,10):
+# Get silhoutte for differente clusters
+for i in range(2,10):
     model = TimeSeriesKMeans(n_clusters=i, metric="dtw", max_iter=10).fit(X_train)
     labels = model.labels_
     silhouette.append(silhouette_score(X_train, labels, metric="dtw") )
+
+# Build model with optimum number of clusters according to sihouette results
+model = TimeSeriesKMeans(n_clusters=3, metric="dtw", max_iter=10).fit(X_train)
+etiquetas = model.labels_ 
+
+# Analizar comportamiento de cada variable de cada cluster
+# Analizar que subio/bajo/mantuvo y comaprar para que variables comparten este comportamiento en el cluster,
+# Es decir comportamiento característico de cada cluster. 
+# Una vez identificados los comportamientos carctarísticos de cada cluster indentificar 
+# los eventos importantes, a qué cluster pertenecen y lo que esto dice de esos eventos. 
+
+data_cluster = pd.DataFrame(data_c)
+data_cluster['Cluster'] = etiquetas
+
+# Split data by clusters
+cluster_1 = data_cluster[data_cluster['Cluster']==0]
+
+# Get date differences to identify time jumps
+cluster_1['datedif'] = cluster_1.index.to_series().diff().dt.days
+
+# Get postion of time jumpys
+saltos = cluster_1[cluster_1['datedif']> 1]
+
+# Get index of time jumps
+lista_indice = saltos.index.to_list()
+
+# For each index get location (optimize)
+index_pos = []
+for i in range(len(lista_indice)):
+    pos = lista_indice[i]
+    index_pos.append(cluster_1.index.get_loc(pos))
+
+# Initilize lists for variable anlysis
+
+cambios = []
+
+# Intialize for
+for i in range(len(index_pos)-1):
+
+    # substract last minus first to get change in variables
+    resta = (cluster_1.iloc[index_pos[i+1],0:-1] - cluster_1.iloc[index_pos[i],0:-1]).to_list()
+
+    # Assign change values according to 1, -1, 0
+    result = [1 if j>0 else -1 if j<0 else 0 for j in resta]
+
+    cambios.append(result)
+    
+    # if the first time jump is not in the second position 
+    if index_pos[0] != 1 & i == 0:
+        resta = (cluster_1.iloc[0,0:-1] - cluster_1.iloc[index_pos[i],0:-1]).to_list()
+
+         # Assign change values according to 1, -1, 0
+        result = [1 if j>0 else -1 if j<0 else 0 for j in resta]
+
+        cambios.append(result)
+
+    # if the last position is not the end of the dataframe
+    if lista_indice[-1] != len(cluster_1) & i == len(index_pos)-2:
+        resta = (cluster_1.iloc[index_pos[i+1],0:-1] - cluster_1.iloc[-1,0:-1]).to_list()
+
+         # Assign change values according to 1, -1, 0
+        result = [1 if j>0 else -1 if j<0 else 0 for j in resta]
+
+        cambios.append(result)
+
+cambios_df = pd.DataFrame(cambios, columns=cluster_1.columns.to_list()[0:-1])
+summary_cambios = cambios_df.apply(pd.Series.value_counts)
+
+
+
+
+
+
+cluster_2 = data_cluster[data_cluster['Cluster']==1]
+cluster_2['datedif'] = cluster_2.index.to_series().diff()
+diff_c2 = cluster_2[cluster_2.columns.to_list()[:-2]].diff()
+diff_c2['datedif'] = cluster_2['datedif']
+diff_c2.dropna(inplace=True)
+
+
+
+cluster_3 = data_cluster[data_cluster['Cluster']==2]
+cluster_3['datedif'] = cluster_3.index.to_series().diff()
+diff_c3 = cluster_3[cluster_3.columns.to_list()[:-2]].diff()
+diff_c3['datedif'] = cluster_3['datedif']
+diff_c3.dropna(inplace=True)
+
+
+
