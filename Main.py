69411c21a62b0ecde068de28a9231d6574fab4f5
renattaGS <renattags20@gmail.com>
2021-11-11 13:54:25 -0600

    Comportamiento clusters

--- Main.py
+++ Main.py
@@ -9,96 +9,13 @@ data_o = dt.data_o
 data_c = fn.clean_data(data_o, 'Fecha')
 
 # Obtener datos para el óptimo número de clusters
-silhouette = fn.optimum_clusters(data_c, 8)
-
-
-
+silhouette, num_c = fn.optimum_clusters(data_c, 8)
 
 # Build model with optimum number of clusters according to sihouette results
-model = TimeSeriesKMeans(n_clusters=3, metric="dtw", max_iter=10).fit(X_train)
-etiquetas = model.labels_ 
+separate_clusters = fn.clusters(data_c, num_c[0])
 
-# Analizar comportamiento de cada variable de cada cluster
-# Analizar que subio/bajo/mantuvo y comaprar para que variables comparten este comportamiento en el cluster,
-# Es decir comportamiento característico de cada cluster. 
-# Una vez identificados los comportamientos carctarísticos de cada cluster indentificar 
-# los eventos importantes, a qué cluster pertenecen y lo que esto dice de esos eventos. 
-
-data_cluster = pd.DataFrame(data_c)
-data_cluster['Cluster'] = etiquetas
-
-# Split data by clusters
-cluster_1 = data_cluster[data_cluster['Cluster']==0]
-
-# Get date differences to identify time jumps
-cluster_1['datedif'] = cluster_1.index.to_series().diff().dt.days
-
-# Get postion of time jumpys
-saltos = cluster_1[cluster_1['datedif']> 1]
-
-# Get index of time jumps
-lista_indice = saltos.index.to_list()
-
-# For each index get location (optimize)
-index_pos = []
-for i in range(len(lista_indice)):
-    pos = lista_indice[i]
-    index_pos.append(cluster_1.index.get_loc(pos))
-
-# Initilize lists for variable anlysis
-
-cambios = []
-
-# Intialize for
-for i in range(len(index_pos)-1):
-
-    # substract last minus first to get change in variables
-    resta = (cluster_1.iloc[index_pos[i+1],0:-1] - cluster_1.iloc[index_pos[i],0:-1]).to_list()
-
-    # Assign change values according to 1, -1, 0
-    result = [1 if j>0 else -1 if j<0 else 0 for j in resta]
-
-    cambios.append(result)
-    
-    # if the first time jump is not in the second position 
-    if index_pos[0] != 1 & i == 0:
-        resta = (cluster_1.iloc[0,0:-1] - cluster_1.iloc[index_pos[i],0:-1]).to_list()
-
-         # Assign change values according to 1, -1, 0
-        result = [1 if j>0 else -1 if j<0 else 0 for j in resta]
-
-        cambios.append(result)
-
-    # if the last position is not the end of the dataframe
-    if lista_indice[-1] != len(cluster_1) & i == len(index_pos)-2:
-        resta = (cluster_1.iloc[index_pos[i+1],0:-1] - cluster_1.iloc[-1,0:-1]).to_list()
-
-         # Assign change values according to 1, -1, 0
-        result = [1 if j>0 else -1 if j<0 else 0 for j in resta]
-
-        cambios.append(result)
-
-cambios_df = pd.DataFrame(cambios, columns=cluster_1.columns.to_list()[0:-1])
-summary_cambios = cambios_df.apply(pd.Series.value_counts)
-
-
-
-
-
-
-cluster_2 = data_cluster[data_cluster['Cluster']==1]
-cluster_2['datedif'] = cluster_2.index.to_series().diff()
-diff_c2 = cluster_2[cluster_2.columns.to_list()[:-2]].diff()
-diff_c2['datedif'] = cluster_2['datedif']
-diff_c2.dropna(inplace=True)
-
-
-
-cluster_3 = data_cluster[data_cluster['Cluster']==2]
-cluster_3['datedif'] = cluster_3.index.to_series().diff()
-diff_c3 = cluster_3[cluster_3.columns.to_list()[:-2]].diff()
-diff_c3['datedif'] = cluster_3['datedif']
-diff_c3.dropna(inplace=True)
+# Caracterización del cportamiento de cada cluster
+comportamiento = fn.comportamiento_clusters(separate_clusters)
 
 
 
--- functions.py
+++ functions.py
@@ -1,8 +1,6 @@
 
 
 import pandas as pd
-import numpy as np
-import tslearn
 from tslearn.preprocessing import TimeSeriesScalerMeanVariance
 from tslearn.clustering import TimeSeriesKMeans, silhouette_score
 
@@ -23,8 +21,85 @@ def optimum_clusters(df: pd.DataFrame, clusters: int):
 
     # Get silhoutte for differente clusters
     for i in range(2,clusters):
-        model = TimeSeriesKMeans(n_clusters=i, metric="dtw", max_iter=10).fit(X_train)
+        model = TimeSeriesKMeans(n_clusters=i, metric="dtw", max_iter=20).fit(X_train)
         labels = model.labels_
         silhouette.append(silhouette_score(X_train, labels, metric="dtw"))
     
-    return silhouette
+    return silhouette, pd.DataFrame(silhouette).idxmax()
+
+def clusters(df: pd.DataFrame, num_clusters):
+
+    X_train = TimeSeriesScalerMeanVariance().fit_transform(df)
+    model = TimeSeriesKMeans(n_clusters=num_clusters, metric="dtw", max_iter=20).fit(X_train)
+    df['Clusters'] = model.labels_ 
+
+    separate_df = {}
+    for i in range(num_clusters):
+        separate_df['cluster_'+ str(i)] = df[df['Clusters'] == i]
+
+    return separate_df
+
+def comportamiento_clusters(dict_clusters: dict):
+
+    dict_comportamiento = {}
+
+    
+
+    for i in range(len(dict_clusters)):
+        print('cluster_'+ str(i))
+        cluster = dict_clusters['cluster_' + str(i)]
+        cluster['datedif'] = cluster.index.to_series().diff().dt.days
+        saltos = cluster[cluster['datedif']>1]
+        indice_saltos = saltos.index.to_list()
+        
+        index_pos = []
+
+        for i in indice_saltos:
+            index_pos.append(cluster.index.get_loc(i))
+
+
+        cambios = []
+
+    # Intialize for
+        for i in range(len(index_pos)-1):
+
+        # substract last minus first to get change in variables
+            resta = (cluster.iloc[index_pos[i+1],0:-1] - cluster.iloc[index_pos[i],0:-1]).to_list()
+
+        # Assign change values according to 1, -1, 0
+            result = [1 if j>0 else -1 if j<0 else 0 for j in resta]
+
+            cambios.append(result)
+        
+        # if the first time jump is not in the second position 
+            if index_pos[0] != 1 & i == 0:
+                resta = (cluster.iloc[0,0:-1] - cluster.iloc[index_pos[i],0:-1]).to_list()
+
+                # Assign change values according to 1, -1, 0
+                result = [1 if j>0 else -1 if j<0 else 0 for j in resta]
+
+                cambios.append(result)
+
+            # if the last position is not the end of the dataframe
+            if index_pos[-1] != len(cluster) & i == len(index_pos)-2:
+
+                resta = (cluster.iloc[index_pos[i+1],0:-1] - cluster.iloc[-1,0:-1]).to_list()
+
+                # Assign change values according to 1, -1, 0
+                result = [1 if j>0 else -1 if j<0 else 0 for j in resta]
+
+                cambios.append(result)
+
+        cambios_df = pd.DataFrame(cambios, columns=cluster.columns.to_list()[0:-1])
+        summary_cambios = cambios_df.apply(pd.Series.value_counts)
+        dict_comportamiento['cluster' + str(i)] = summary_cambios
+
+
+ 
+
+    return dict_comportamiento
+
+
+
+
+
